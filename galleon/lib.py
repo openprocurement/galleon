26c074784382adf4cbfb2d848209fe5717bf7f27
vladkhard <vladkhard@gmail.com>
2018-06-22 16:28:34 +0300

    replace some jq queries with python functions

--- galleon/lib.py
+++ galleon/lib.py
@@ -1,4 +1,1 @@
-UNIQ = ".|unique"
 INITIALIZE = '. as $data | $data.{path} = (if $data.{path} then $data.{path} else {value} end)'
-TAG_OCDS = '. as $data | $data.tags |= ["{default}"]|.tags += (if $data.contracts then ["contract"] else [] end)|.tags += (if $data.awards then ["award"] else [] end)'
-TAG_ROLES = '.{path} | if type=="array" then map(.roles+=["{role}"]) else .roles+=["{role}"] end'
